25143a895bc1964e26cc5efd786ba07c2ffb0ace
Yaroslav <asdfc1234987@gmail.com>
2026-03-17 21:48:10 +0200

    v. 0.0.1

---
+++ Laba2/Program.cs
@@ -0,0 +1,195 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Net;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ввиберіть задачу:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Tasks(n);
+    }
+    static void Tasks(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                Task1();
+                break;
+             
+            case 2:
+                Task2();
+                break;
+
+            case 3:
+                Task3();
+                break;
+
+            case 4:
+                Task4();
+                break;
+
+            default:
+                Console.WriteLine("Такої задачі немає");
+                break;
+        }
+
+
+    }
+    static bool Corect(int rows, int cols, int[] n, int[] k)
+    {
+        int sum = n.Length;
+        int sum1 = k.Length;
+        if (sum > rows || sum < rows)
+        {
+            Console.WriteLine("Ви ввели не правильну кількість рядків");
+            return false;
+        }
+        if (sum1 > cols || sum1 < cols)
+        {
+            Console.WriteLine("Ви ввели не правильну кількість стовпців");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+      static int[,] Print(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++) 
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        return matrix;
+       }
+       static bool Corect2(int n, int c)
+        {
+        if (n == c)
+            return true;
+        else
+            return false;
+        }
+     static int[,] Input(int[,] arr , int rows , int cols)
+    {
+        Console.WriteLine("Введіть дані:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"Введіть елемент {j + 1} у рядку {i + 1} = ");
+                arr[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        return arr;
+    }
+    static void Input2( out int rows ,out int cols)
+    {
+        Console.Write("Введіть кількість рядків: ");
+        rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть кількість стовпців: ");
+        cols = int.Parse(Console.ReadLine());
+    }
+
+    static void Task1()
+    {
+        int rows , cols;
+
+        Input2(out rows, out cols);
+
+        int[,] arr = new int[rows, cols];
+
+        Input(arr, rows, cols);
+
+        int[] ints = new int[rows];
+        int[] ints1 = new int[cols];
+
+        int[,] outcome = new int[rows, 2];
+
+        if (!Corect(rows, cols, ints, ints1))
+        {
+            return;
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            int min = arr[i, 0];
+            int index = 0;
+
+            for (int j = 0; j < cols; j++)
+            {
+                if (arr[i, j] < min)
+                {
+                    min = arr[i, j];
+                    index = j;
+                }
+            }
+
+            outcome[i, 0] = min;
+            outcome[i, 1] = index;
+        }
+        Print(outcome);
+
+
+    }
+   
+    static void Task2()
+    {
+        int rows, cols;
+
+        Input2(out rows, out cols);
+
+        if (!Corect2(rows,cols))
+            {
+            Console.WriteLine("Введіть елементи, щоб матриця була квадратна!!!");
+            return;
+        }
+
+        int[,] arr = new int[rows, cols];
+
+        Console.WriteLine("Введіть дані:");
+
+        Input(arr, rows, cols);
+
+        for (int i = 0; i < rows; i++)
+        {
+            int max = arr[i, 0];
+            int this_row = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                while (arr[i, j] > max)
+                {
+                    max = arr[i, j];
+                    arr[i, j] = this_row;
+                    this_row++;
+
+                }
+            }
+        }
+        int row = arr.GetLength(0);
+
+        int []needrow = new int[cols];
+        for (int i = rows - 1, j = 0; i >= 0; i--, j++)
+        {
+            needrow[j]= arr[i, j];
+        }
+
+    }
+    static void Task3()
+    {
+
+    }
+    static void Task4()
+    {
+
+    }
+}
